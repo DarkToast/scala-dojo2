327fabe50f6718d00847eb08acac049672917c88
Christian Iwanzik <c.iwanzik@tarent.de>
2016-03-31 16:21:36 +0200

    First mastermind spec to define the package structure

---
+++ src/test/scala/mastermind/christian/MastermindSpec.scala
@@ -0,0 +1,11 @@
+package mastermind.christian
+
+import org.scalatest.{FeatureSpec, GivenWhenThen, MustMatchers}
+
+
+class MastermindSpec extends FeatureSpec with GivenWhenThen with MustMatchers {
+
+  feature("Winning a Point Increases Score Correctly") {
+
+  }
+}
